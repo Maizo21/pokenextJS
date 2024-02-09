3b172f9099157839577ede312998480b10a949d9
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2024-02-09 20:32:52 -0300

    Corregir etiqueta de marco en página de juego

--- app/game/page.jsx
+++ app/game/page.jsx
@@ -6,7 +6,7 @@ const Game = () => {
       <h2>Pokemon Esmeralda</h2>
       <p className='subtitle'>Besto game without doubt</p>
       <section className='game-container'>
-        <iframe src="https://www.retrogames.cc/embed/45104-pokemon-exceeded-v3-3.html" frameborder="no"></iframe>
+        <iframe src="https://www.retrogames.cc/embed/45104-pokemon-exceeded-v3-3.html"></iframe>
       </section>
 
     </>
