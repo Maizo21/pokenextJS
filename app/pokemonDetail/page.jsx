d34f625da6801d8ea86eb6ac8d44e368822c3225
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2024-02-09 20:59:00 -0300

    Actualizar página de detalles de Pokémon

--- app/pokemonDetail/page.jsx
+++ app/pokemonDetail/page.jsx
@@ -11,8 +11,7 @@ const pokemonDetail = () => {
   const [pokemon, setPokemon] = useState('');
 
   const searchParams = useSearchParams()
-  const pokemonID = searchParams.get('id');
-
+  let pokemonID = searchParams.get('id');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,44 +29,42 @@ const pokemonDetail = () => {
       <h1>Pokemon Detail</h1>
 
       <main>
+        <Suspense fallback={<div>Loading...</div>}>
+          {pokemon && (
+            <section className='pokemon-details' >
+              <h2>Here is the detail of {pokemon.name}</h2>
+              <p>ID: {pokemon.id}</p>
+              <p>Altura: {pokemon.height}</p>
+              <p>Peso: {pokemon.weight}</p>
+              <p>Experiencia base: {pokemon.base_experience}</p>
+              {pokemon.types && (
+                <p>Tipo: {pokemon.types.map((type, index) => (
+                  <span key={index}>{type.type.name}, </span>
+                ))}</p>
+              )}
 
-        <Suspense fallback={<div>Cargango...</div>}></Suspense>
+              {pokemon.stats && (
+                <p>Estadisticas: {pokemon.stats.map((stat, index) => (
+                  <span key={index}>{stat.stat.name}: {stat.base_stat}, </span>
+                ))}</p>
+              )}
+              <p>Picture:</p>
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png`} alt={`Pokemon ${pokemonID} picture`} />
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonID}.png`} alt={`Pokemon ${pokemonID} picture`} />
 
+              {pokemon.cries && (
+                <>
+                  <p>Sonido:</p>
+                  <audio controls autoPlay>
+                    <source src={pokemon.cries.latest != undefined ? pokemon.cries.latest : pokemon.cries.legacy  } type="audio/mpeg" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </>
+              )}
 
-        {pokemon && (
-          <section className='pokemon-details' >
-            <h2>Here is the detail of {pokemon.name}</h2>
-            <p>ID: {pokemon.id}</p>
-            <p>Altura: {pokemon.height}</p>
-            <p>Peso: {pokemon.weight}</p>
-            <p>Experiencia base: {pokemon.base_experience}</p>
-            {pokemon.types && (
-              <p>Tipo: {pokemon.types.map((type, index) => (
-                <span key={index}>{type.type.name}, </span>
-              ))}</p>
-            )}
-
-            {pokemon.stats && (
-              <p>Estadisticas: {pokemon.stats.map((stat, index) => (
-                <span key={index}>{stat.stat.name}: {stat.base_stat}, </span>
-              ))}</p>
-            )}
-            <p>Picture:</p>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png`} alt={`Pokemon ${pokemonID} picture`} />
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonID}.png`} alt={`Pokemon ${pokemonID} picture`} />
-
-            {pokemon.cries && (
-              <>
-                <p>Sonido:</p>
-                <audio controls autoPlay>
-                  <source src={pokemon.cries.latest != undefined ? pokemon.cries.latest : pokemon.cries.legacy  } type="audio/mpeg" />
-                  Your browser does not support the audio element.
-                </audio>
-              </>
-            )}
-
-          </section>
-        )}
+            </section>
+          )}
+        </Suspense>
       </main>
 
     </>
